3294e8cc32cacbac381ca1403e5db8bc72215bb9
LinXiaoWei <linxiaowei.me@gmail.com>
2021-05-06 18:40:22 +0800

    update project

---
+++ lin-boot-common/src/main/java/me/linxiaowei/boot/entity/BaseEntity.java
@@ -0,0 +1,26 @@
+package me.linxiaowei.boot.entity;
+
+import java.util.Date;
+
+/**
+ * BaseEntity
+ *
+ * @author <a href="mailto://linxiaowei.me@gmail.com">LinXiaoWei</a>
+ * @date 2021/5/6 16:40
+ */
+public class BaseEntity {
+
+    /**
+     * 自增 id
+     */
+    private Integer id;
+    /**
+     * 创建时间
+     */
+    private Date gmtCreate;
+    /**
+     * 修改时间
+     */
+    private Date gmtModified;
+
+}
