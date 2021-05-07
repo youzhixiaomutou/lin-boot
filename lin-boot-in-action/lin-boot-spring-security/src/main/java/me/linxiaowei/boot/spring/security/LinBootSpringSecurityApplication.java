1513d45af3671c393764db3b00c52afb1adba765
LinXiaoWei <linxiaowei.me@gmail.com>
2021-05-07 15:03:00 +0800

    add lin-boot-spring-security

---
+++ lin-boot-in-action/lin-boot-spring-security/src/main/java/me/linxiaowei/boot/spring/security/LinBootSpringSecurityApplication.java
@@ -0,0 +1,19 @@
+package me.linxiaowei.boot.spring.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * LinBootSpringSecurityApplication
+ *
+ * @author <a href="mailto://linxiaowei.me@gmail.com">LinXiaoWei</a>
+ * @date 2021/5/7 14:53
+ */
+@SpringBootApplication
+public class LinBootSpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinBootSpringSecurityApplication.class, args);
+    }
+
+}
---
+++ lin-boot-in-action/lin-boot-spring-security/src/test/java/me/linxiaowei/boot/spring/security/LinBootSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package me.linxiaowei.boot.spring.security;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * LinBootSpringSecurityApplicationTests
+ *
+ * @author <a href="mailto://linxiaowei.me@gmail.com">LinXiaoWei</a>
+ * @date 2021/5/7 14:53
+ */
+@SpringBootTest
+public class LinBootSpringSecurityApplicationTests {
+}
