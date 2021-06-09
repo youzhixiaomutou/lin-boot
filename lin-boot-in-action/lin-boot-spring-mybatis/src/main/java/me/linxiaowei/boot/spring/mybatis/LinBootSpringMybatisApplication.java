b9348d118d99129cec4fc7a723e8017249886ecd
LinXiaoWei <linxiaowei.me@gmail.com>
2021-06-09 10:39:44 +0800

    add lin-boot-spring-mybatis

---
+++ lin-boot-in-action/lin-boot-spring-mybatis/src/main/java/me/linxiaowei/boot/spring/mybatis/LinBootSpringMybatisApplication.java
@@ -0,0 +1,19 @@
+package me.linxiaowei.boot.spring.mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * LinBootSpringMybatisApplication
+ *
+ * @author <a href="mailto://linxiaowei.me@gmail.com">LinXiaoWei</a>
+ * @date 2021/6/9 10:37
+ */
+@SpringBootApplication
+public class LinBootSpringMybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinBootSpringMybatisApplication.class, args);
+    }
+
+}
---
+++ lin-boot-in-action/lin-boot-spring-mybatis/src/test/java/me/linxiaowei/boot/spring/mybatis/LinBootSpringMybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package me.linxiaowei.boot.spring.mybatis;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * LinBootSpringMybatisApplicationTests
+ *
+ * @author <a href="mailto://linxiaowei.me@gmail.com">LinXiaoWei</a>
+ * @date 2021/6/9 10:38
+ */
+@SpringBootTest
+public class LinBootSpringMybatisApplicationTests {
+}
